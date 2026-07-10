e5c0700ea050ddb0e0d7a93533094ef22d87a26d
Tannu234 <tamannabaghel322@gmail.com>
2026-07-11 01:41:39 +0530

    feat: improve payment processing

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
@@ -55,18 +55,18 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({ url: session.url });
 });
 
-// exports.paymentDetails = catchAsyncErrors(async (req, res, next) => {
-//   const session = await stripe.checkout.sessions.retrieve(
-//     "cs_test_b1wjqczdc5wwaNj5FjvxipLWeKZIvZQvsbC2OjfC5FEZw5vJ8aJbdMPRYC",
-//     {
-//       expand: ["customer"],
-//     }
-//   );
-// console.log(session);
-//   res.json({
-//     session,
-//   });
-// });
+exports.paymentDetails = catchAsyncErrors(async (req, res, next) => {
+  const session = await stripe.checkout.sessions.retrieve(
+     "cs_test_b1wjqczdc5wwaNj5FjvxipLWeKZIvZQvsbC2OjfC5FEZw5vJ8aJbdMPRYC",
+     {
+       expand: ["customer"],
+     }
+   );
+ console.log(session);
+   res.json({
+     session,
+   });
+ });
 
 // Send stripe API Key   =>   /api/v1/stripeapi
 exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
@@ -74,78 +74,3 @@ exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
     stripeApiKey: process.env.STRIPE_API_KEY,
   });
 });
-=======
-const catchAsyncErrors = require("../middlewares/catchAsynErros")
-
-const dotenv = require("dotenv")
-
-dotenv.config({path:"./config/config.env"})
-
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-console.log("KEY",process.env.STRIPE_SECRET_KEY)
-
-//process payment api
-
-exports.processPayment = catchAsyncErrors(async(req,res,next)=>{
-    console.log(req.body)
-
-    //create stripe checkout session
-
-    const session = await stripe.checkout.session.create({
-        customer_email:req.user.email,
-        phone_number_collection:{
-            enabled:true,
-        },
-        line_items: req.body.items.map((items)=>({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                   name:item.foodItem.name,
-                   images:[item.foodItem.image[0].url],
-                
-            },
-            //rs100 becomes 10000 paise
-                   unit_amount: item.foodItem.price * 100
-            
-        },
-        quantity:item.quantity
-        })),
-
-        mode:"payment",
-        shipping_address_collection:{
-            allowed_countries:["US","IN"]
-        },
-        shipping_options:[{
-            shipping_rate_data:{
-                dispaly_name:"Delivery Charges",
-                type:"fixed_amount",
-                fixed_amount:{
-                    amount:5500, //amonut in paise
-                    currency:"inr"
-                },
-                delivery_estimated:{
-                    minimum:{
-                        unit:"hour",
-                        value:1
-                    },
-                    maximum:{
-                        unit:"hour",
-                        value:3
-                    }
-                }
-            }
-        }],
-        success_url:'${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url:'${process.env.FRONTEND_URL}/cart'
-
-    });
-    res.status(200).json({url:session.url})
-})
-
-//send stripe api key
-exports.sendStripeApi = catchAsyncErrors(async(req,res,next)=>{
-    res.status(200).json({
-        stripeApiKey:process.env.STIPR_API_KEY
-    })
-})
->>>>>>> 10383afece3d9e043e08a3473b80fb0d32bd2897
