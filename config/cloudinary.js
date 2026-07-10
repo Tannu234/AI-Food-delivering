21778cc102fa8cfffc0fd67841d2401f5e5e402f
Tannu234 <tamannabaghel322@gmail.com>
2026-07-11 01:40:04 +0530

    feat: configure Cloudinary integration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -8,14 +8,4 @@ cloudinary.config({
 });
 
 module.exports = cloudinary;
-=======
-const cloudinary = require('cloudinary');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-module.exports = cloudinary;
->>>>>>> 10383afece3d9e043e08a3473b80fb0d32bd2897
