db9e28d272b5732deec2f4bee0866c97d5660b2a
Tannu234 <tamannabaghel322@gmail.com>
2026-07-11 01:40:20 +0530

    feat: improve user authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const User = require("../models/user");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
@@ -80,82 +79,6 @@ exports.login = catchAsyncErrors(async (req, res, next) => {
 });
 
 
-=======
-// ✅ Pehle yeh sab imports
-const User = require('../models/User');
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-const sendToken = require('../utils/sendToken');
-const cloudinary = require('cloudinary').v2;
-
-// ✅ Phir controller
-exports.signupController = catchAsyncErrors(async (req, res, next) => {
-    console.log("=== NEW CODE CHAL RAHA HAI ===");
-    console.log(req.body);
-
-    const { name, email, password, passwordConfirm, phoneNumber } = req.body;
-
-    if (!password) {
-        return res.status(400).json({ message: "Please enter your password" });
-    }
-
-    if (password !== passwordConfirm) {
-        return res.status(400).json({ message: "Passwords do not match" });
-    }
-
-    let avatar = {};
-
-    if (!req.body.avatar || req.body.avatar === "/images/images.png") {
-        avatar = {
-            public_id: "default",
-            url: "/images/images.png"
-        };
-    } else {
-        const result = await cloudinary.uploader.upload(req.body.avatar, {
-            folder: "avatars"
-        });
-        avatar = {
-            public_id: result.public_id,
-            url: result.secure_url
-        };
-    }
-
-    const user = await User.create({
-        name,
-        email,
-        password,
-        phoneNumber,
-        avatar
-    });
-
-    sendToken(user, 200, res);
-});
-//login
-
-exports.login = catchAsyncErrors(async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return next(new Error("Please enter email and password", 400));
-    }
-
-   const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-        return next(new Error("Invalid email or password", 401));
-    }
-    const isPasswordMatched = await user.comparePassword(password, user.password);
-
-    if (!isPasswordMatched){
-        return next(new errorHandler("invalid email or password",401))
-
-    }
-    sendToken(user, 200, res);
-    
-    const isMatch = await user.comparePassword(req.body.password);
-});
-
-
-//update...............
-
->>>>>>> 10383afece3d9e043e08a3473b80fb0d32bd2897
 // Protect Route
 exports.protect = catchAsyncErrors(async (req, res, next) => {
 
@@ -375,9 +298,4 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     message: "Logged out",
   });
 
-<<<<<<< HEAD
 });
-=======
-});
-
->>>>>>> 10383afece3d9e043e08a3473b80fb0d32bd2897
